b0a9dfb4c2f83d399750b1076e39570a7a8416b5
Vitali Skarbinski <sfc-ruper@mail.ru>
2022-01-26 16:38:16 +0300

    init: finish towel-sort Task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  if (arguments[0] === undefined || arguments[0] === null || arguments[0] === '' || arguments[0] === NaN) {
+    return matrix = [];
+  } else if (matrix.length === 0) {
+    return matrix = [];
+  } else if (matrix.length === 2) {
+    return matrix[0].concat(matrix[1].sort((a, b) => b - a));
+  } else if (matrix.length === 3) {
+    return matrix[0].concat(matrix[1].sort((a, b) => b - a), matrix[2]);
+  } else if (matrix.length === 4) {
+    return matrix[0].concat(matrix[1].sort((a, b) => b - a), matrix[2], matrix[3].sort((a, b) => b - a));
+  }
 }
+
+
